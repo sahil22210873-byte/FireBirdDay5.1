4db2193b1807b7904bc12589d28968cc0b329da6
sahil22210873-byte <sahil.22210873@viit.ac.in>
2025-11-28 23:43:37 +0530

    28.final
    intro complete

--- Assets/Intro Audio/IntroSceneController.cs
+++ Assets/Intro Audio/IntroSceneController.cs
@@ -1,3 +1,70 @@
+//using System.Collections;
+//using UnityEngine;
+
+//public class IntroSceneController : MonoBehaviour
+//{
+//    [Header("References")]
+//    public AudioSource introAudio;
+//    public GameObject canvas;
+//    public Animator gateAnimator;
+//    public Transform player;
+
+//    [Header("Trigger")]
+//    public Collider doorTriggerCollider;
+
+//    bool gateOpened = false;
+//    bool gateClosed = false;
+
+//    void Start()
+//    {
+//        if (canvas != null) canvas.SetActive(false);
+
+//        if (introAudio != null)
+//        {
+//            introAudio.Play();
+//            StartCoroutine(ShowCanvasAfterAudio());
+//        }
+//        else
+//        {
+//            if (canvas != null) canvas.SetActive(true);
+//        }
+//    }
+
+//    IEnumerator ShowCanvasAfterAudio()
+//    {
+//        yield return new WaitWhile(() => introAudio != null && introAudio.isPlaying);
+//        yield return new WaitForSeconds(0.25f);
+
+//        if (canvas != null) canvas.SetActive(true);
+//    }
+
+//    public void OnLetsGoClicked()
+//    {
+//        Debug.Log("BUTTON CLICKED!!!");
+//        if (gateAnimator != null)
+//        {
+//            gateAnimator.SetTrigger("Open");
+//            gateOpened = true;
+//        }
+
+//        if (canvas != null) canvas.SetActive(false);
+
+
+//    }
+
+//    public void PlayerEnteredDoorway()
+//    {
+//        if (gateOpened && !gateClosed)
+//        {
+//            if (gateAnimator != null)
+//            {
+//                gateAnimator.SetTrigger("Close");
+//                gateClosed = true;
+//            }
+//        }
+//    }
+//}
+
 using System.Collections;
 using UnityEngine;
 
@@ -18,6 +85,12 @@ public class IntroSceneController : MonoBehaviour
     void Start()
     {
         if (canvas != null) canvas.SetActive(false);
+
+        if (doorTriggerCollider != null)
+        {
+            doorTriggerCollider.enabled = false;
+            Debug.Log("Door trigger disabled at start");
+        }
 
         if (introAudio != null)
         {
@@ -40,6 +113,8 @@ public class IntroSceneController : MonoBehaviour
 
     public void OnLetsGoClicked()
     {
+        Debug.Log("BUTTON CLICKED!!!");
+
         if (gateAnimator != null)
         {
             gateAnimator.SetTrigger("Open");
@@ -47,6 +122,12 @@ public class IntroSceneController : MonoBehaviour
         }
 
         if (canvas != null) canvas.SetActive(false);
+
+        if (doorTriggerCollider != null)
+        {
+            doorTriggerCollider.enabled = true;
+            Debug.Log("Door trigger enabled after button click");
+        }
     }
 
     public void PlayerEnteredDoorway()
